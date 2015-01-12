feac3242c660add0964025842d07d914b7c62c1d
magicant <magicant@9a5e03f8-c55e-0410-a490-9d27d325e308>
2015-01-12 07:06:31 +0000

    settings: add clone.sh
    git-svn-id: https://subversion.assembla.com/svn/magicant/settings@362 9a5e03f8-c55e-0410-a490-9d27d325e308

---
+++ clone.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# https://raw.githubusercontent.com/magicant/settings/master/clone.sh | sh
+
+set -Ceu
+
+if [ "$PWD" != ~/.settings ] && [ "${1-}" != -f ]; then
+  printf 'Use -f to run in a non-standard directory.\n'
+  exit 1
+fi
+
+set -x
+
+git init
+git svn init --prefix=origin/ https://subversion.assembla.com/svn/magicant/settings
+git remote add github git@github.com:magicant/settings.git
+git config remote.github.push refs/remotes/origin/git-svn:refs/heads/master
+git remote update github
+git update-ref refs/remotes/origin/git-svn refs/remotes/github/master
+git remote set-head github master
+git remote set-head origin git-svn
+git reset --hard origin
+git svn rebase
