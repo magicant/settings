e3573aebec5c6e95d040d3ac052336e30baff373
magicant <magicant@9a5e03f8-c55e-0410-a490-9d27d325e308>
2015-06-03 09:29:00 +0000

    settings: pull using HTTPS, to allow pull w/o SSH key
    git-svn-id: https://subversion.assembla.com/svn/magicant/settings@369 9a5e03f8-c55e-0410-a490-9d27d325e308

--- clone.sh
+++ clone.sh
@@ -13,7 +13,8 @@ set -x
 
 git init
 git svn init --prefix=origin/ https://subversion.assembla.com/svn/magicant/settings
-git remote add github git@github.com:magicant/settings.git
+git remote add github https://github.com/magicant/settings.git
+git config remote.github.pushurl git@github.com:magicant/settings.git
 git config remote.github.push refs/remotes/origin/git-svn:refs/heads/master
 git remote update github
 git update-ref refs/remotes/origin/git-svn refs/remotes/github/master
